1dae0596876552a13dfe6cfd0fe89cebc0a77594
Jaafar Mehrez <32996178+JaafarMehrez@users.noreply.github.com>
2026-06-16 09:06:52 +0800

    update files for the question (Why do my HLC contributions for the O-atom IE differ from Klopper et al. at the CCSDT level, while C and Ne agree?)

---
+++ 035/neon-atom/neutral/dawCV5Z/RHF/input.py
@@ -0,0 +1,50 @@
+import pyscf
+from pyscf import cc, lib, tools, scf, symm, ao2mo
+from pyscf.gto.basis import parse_gaussian
+from pyscf.scf import atom_hf
+
+name = 'out'
+mol = pyscf.M(
+    atom = '''
+        NE
+    ''',
+    unit = 'angstrom',
+    basis = {
+            'NE' : parse_gaussian.load('Ne-d-aug-cc-pwCV5Z.gbs', 'NE')
+    },
+    charge = 0,
+    spin = 0,
+    symmetry = True,
+    verbose = 9,
+    symmetry_subgroup = 'D2h',
+    output = name +'.txt',
+    max_memory = 64000,
+)
+
+original_AtomSphAverageRHF = atom_hf.AtomSphAverageRHF
+
+class CustomAtomSphAverageRHF(original_AtomSphAverageRHF):
+    def __init__(self, mol):
+        super().__init__(mol)
+        self.max_cycle = 9999
+        self.direct_scf = False
+
+atom_hf.AtomSphAverageRHF = CustomAtomSphAverageRHF
+
+mf = mol.RHF().set(
+    conv_tol=1e-13,
+    max_cycle=9999,
+    ddm_tol=1e-12,
+    direct_scf=False,
+    chkfile=name + '.chk',
+    init_guess='atom',
+    irrep_nelec={'Ag': 4, 'B3u': 2, 'B2u': 2, 'B1u': 2}
+)
+
+mf.kernel()
+
+atom_hf.AtomSphAverageRHF = original_AtomSphAverageRHF
+
+#pyscf.tools.fcidump.from_chkfile_uhf('FCIdump',name+'.chk',tol=1e-18, float_format='% 0.20E',molpro_orbsym=False,orbsym=None)
+pyscf.tools.fcidump.from_scf(mf,'fort.55',tol=1e-18, float_format='% 0.20E',molpro_orbsym=False)
+#MRCCInterface.run_mrcc(mf, fort_file='fort.55', mrcc_input_file='MINP', tol=1e-18, float_format='% 0.20E', molpro_orbsym=False)
